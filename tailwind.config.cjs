cb6c25ecd8531413c5dae787e836f14e893bc3bd
jumaniyozov <jumaniyozovse@gmail.com>
2023-03-10 17:43:07 +0500

    light mode done v1

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#2A2D31'
+        },
+        secondary: {
+          500: '#FFFDF9'
+        }
+      },
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+        'konstant': ['Kontsant Grotesk'],
+      }
+    },
+  },
+  plugins: [],
+}
